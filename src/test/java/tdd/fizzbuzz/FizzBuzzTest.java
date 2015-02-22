c3c595cff4f00b24e4eec3175bd59333a4a750a9
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2015-02-22 14:40:58 +0100

    Added another test

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,15 +1,26 @@
 package tdd.fizzbuzz;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import tdd.fizbuzz.FizzBuzz;
 import tdd.fizbuzz.FizzBuzzImpl;
 
+import java.net.MalformedURLException;
+
 import static com.googlecode.catchexception.CatchException.catchException;
 import static com.googlecode.catchexception.CatchException.caughtException;
+import static junitparams.JUnitParamsRunner.$;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "FizzBuzz";
     private FizzBuzz fizzBuzz;
 
     @Before
@@ -21,6 +32,25 @@ public class FizzBuzzTest {
     public void shouldThrowExceptionWhenNIsLessThanZero() {
         catchException(fizzBuzz).say(-1);
         assertTrue(caughtException() instanceof IllegalArgumentException);
+    }
+
+    @Test
+    @Parameters(method = "provideNumbersDividableByThree")
+    public void shouldReturnFizzWhenNIsDividableByThree(int n) {
+        fizzBuzz.say(n);
+        assertEquals(FIZZ, fizzBuzz.say(n));
+    }
+
+    private Object[] provideNumbersDividableByThree() {
+        return $(
+                $(3),
+                $(6),
+                $(9),
+                $(27),
+                $(54),
+                $(72),
+                $(99)
+        );
     }
 
 }
