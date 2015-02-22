60c55fbe8eaa99e17a14122a43e329137b6f1d18
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2015-02-22 14:30:08 +0100

    First test passes

--- src/main/java/tdd/fizbuzz/FizzBuzzImpl.java
+++ src/main/java/tdd/fizbuzz/FizzBuzzImpl.java
@@ -1,9 +1,12 @@
 package tdd.fizbuzz;
 
+import com.google.common.base.Preconditions;
+
 public class FizzBuzzImpl implements FizzBuzz {
+    private static final String WRONG_ARGUMENT = "Argument is of illegal type!";
 
     @Override
     public void say(int n) {
-
+        Preconditions.checkArgument(n > 0, WRONG_ARGUMENT);
     }
 }
